8c111746ec489a112b2b3daa9676166720a1cae3
Aman Saraf Jain <aman.truba@gmail.com>
2014-09-18 11:27:18 +0530

    Second commit of Cucumber Avalon

---
+++ src/test/MyReporterListenerAdapter.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.List;
+
+import org.testng.IReporter;
+import org.testng.ISuite;
+import org.testng.xml.XmlSuite;
+
+public class MyReporterListenerAdapter implements IReporter {
+
+public void generateReport(List xml, List suites, String outdir) {
+	
+	
+}
+
+
+}
